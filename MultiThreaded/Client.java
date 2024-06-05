f5e2dc97c0ced009ea6872fd14b9646f0e6b6baa
Pratham Goyal <143429722+wakeuppratham@users.noreply.github.com>
2024-06-05 18:04:15 +0530

    Update Client.java

--- MultiThreaded/Client.java
+++ MultiThreaded/Client.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,39 +8,38 @@ import java.net.UnknownHostException;
 
 public class Client {
     
-    public Runnable getRunnable() throws UnknownHostException, IOException {
-        return new Runnable() {
-            @Override
-            public void run() {
-                int port = 8010;
-                try {
-                    InetAddress address = InetAddress.getByName("localhost");
-                    Socket socket = new Socket(address, port);
-                    try (
-                        PrintWriter toSocket = new PrintWriter(socket.getOutputStream(), true);
-                        BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()))
-                    ) {
-                        toSocket.println("Hello from Client " + socket.getLocalSocketAddress());
-                        String line = fromSocket.readLine();
-                        System.out.println("Response from Server " + line);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+    private static class ClientRunnable implements Runnable {
+        @Override
+        public void run() {
+            int port = 8010;
+            try {
+                InetAddress address = InetAddress.getByName("localhost");
+                Socket socket = new Socket(address, port);
                 
+                PrintWriter toSocket = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                toSocket.println("Hello from Client " + socket.getLocalSocketAddress());
+                String line = fromSocket.readLine();
+                System.out.println("Response from Server " + line);
+
+                toSocket.close();
+                fromSocket.close();
+                socket.close();
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        };
+        }
     }
-    
-    public static void main(String[] args){
-        Client client = new Client();
-        for(int i=0; i<50; i++){
-            try{
-                Thread thread = new Thread(client.getRunnable());
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 50; i++) {
+            try {
+                Thread thread = new Thread(new ClientRunnable());
                 thread.start();
-            }catch(Exception ex){
+            } catch (Exception ex) {
                 return;
             }
         }
