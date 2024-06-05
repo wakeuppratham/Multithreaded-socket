853f1cea25cec2a9aaef14e84c84131dabfa50da
Pratham Goyal <143429722+wakeuppratham@users.noreply.github.com>
2024-06-05 18:04:39 +0530

    Update Server.java

--- MultiThreaded/Server.java
+++ MultiThreaded/Server.java
@@ -2,32 +2,31 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.function.Consumer;
 
 public class Server {
 
-    public Consumer<Socket> getConsumer(){
-        return (clientSocket)->{
-            try(PrintWriter toSocket = new PrintWriter(clientSocket.getOutputStream(),true)){
-                toSocket.println("Hello from server " + clientSocket.getInetAddress());
-            }catch(IOException ex){
-                ex.printStackTrace();
-            }
-        };
+    // Method to handle client connection
+    private void handleClient(Socket clientSocket) {
+        try (PrintWriter toSocket = new PrintWriter(clientSocket.getOutputStream(), true)) {
+            toSocket.println("Hello from server " + clientSocket.getInetAddress());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
+
     public static void main(String[] args) {
         int port = 8010;
         Server server = new Server();
-        try{
+        try {
             ServerSocket serverSocket = new ServerSocket(port);
             serverSocket.setSoTimeout(10000);
-            System.out.println("Server is listening on port" + port);
-            while(true){
-                Socket clienSocket = serverSocket.accept();
-                Thread thread = new Thread(()->server.getConsumer().accept(clienSocket));
+            System.out.println("Server is listening on port " + port);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                Thread thread = new Thread(() -> server.handleClient(clientSocket));
                 thread.start();
             }
-        }catch(IOException ex){
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
